1f7f998d564ba06ce4d2c736245c99bca7e92201
Angelo-Lucas <angelolucas123738@gmail.com>
2021-06-30 22:44:56 -0300

    Ultimas alteracoes

--- target/generated-sources/annotations/com/mycompany/enade/model/Questao_.java
+++ target/generated-sources/annotations/com/mycompany/enade/model/Questao_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-30T22:23:28")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-30T22:44:15")
 @StaticMetamodel(Questao.class)
 public class Questao_ { 
 
--- target/generated-sources/annotations/com/mycompany/enade/model/Resultado_.java
+++ target/generated-sources/annotations/com/mycompany/enade/model/Resultado_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-30T22:23:28")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-30T22:44:15")
 @StaticMetamodel(Resultado.class)
 public class Resultado_ { 
 
